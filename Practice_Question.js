d347fd81584b4697263b917ae3c4c08691b60d13
Sonu-Delhi <sy9812672@gmail.com>
2024-06-16 23:38:04 +0530

    add some question

--- Practice_Question.js
+++ Practice_Question.js
@@ -15,19 +15,59 @@
 // 70-79: C
 // 60-69: D
 // Below 60: F
-var score = prompt("Enter your score");
-if (score >= 90 && score <= 100) {
-    console.log("Your grade is A");
-}
-else if (score >= 80 && score <= 89) {
-    console.log("Your grade is B");
-}
-else if (score >= 70 && score <= 79) {
-    console.log("Your grade is C");
-}
-else if (score >= 60 && score <= 69) {
-    console.log("Your grade is D");
-}
-else {
-    console.log("Your grade is F");
-}
+// var score = prompt("Enter your score");
+// if (score >= 90 && score <= 100) {
+//     console.log("Your grade is A");
+// }
+// else if (score >= 80 && score <= 89) {
+//     console.log("Your grade is B");
+// }
+// else if (score >= 70 && score <= 79) {
+//     console.log("Your grade is C");
+// }
+// else if (score >= 60 && score <= 69) {
+//     console.log("Your grade is D");
+// }
+// else {
+//     console.log("Your grade is F");
+// }
+
+// Q-3. Write a code to check whether the given number is even or odd.
+// var num = prompt("Enter a number");
+// if (num % 2 == 0) {
+//     console.log(num + " is even");
+// }
+// else {
+//     console.log(num + " is odd");
+// }
+
+// Q-4. Write a code to check whether the given number is positive or negative.
+// var num = prompt("Enter a number");
+// if (num > 0) {
+//     console.log(num + " is positive");
+// }
+// else if (num < 0) {
+//     console.log(num + " is negative");
+// }
+// else {
+//     console.log(num + " is zero");
+// }
+
+// Q-5. Write a code to check whether the given year is leap year or not.
+// var year = prompt("Enter a year");
+// if (year % 4 == 0) {
+//     if (year % 100 == 0) {
+//         if (year % 400 == 0) {
+//             console.log(year + " is a leap year");
+//         }
+//         else {
+//             console.log(year + " is not a leap year");
+//         }
+//     }
+//     else {
+//         console.log(year + " is a leap year");
+//     }
+// }
+// else {
+//     console.log(year + " is not a leap year");
+// }
